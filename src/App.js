d113b4fa6e18070d4d8bd39e56b966811e9aa9ee
Jozef <Jozefw@sbcglobal.net>
2023-04-24 13:14:39 -0700

    mvin on

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React,{useState, useEffect} from 'react';
+import React,{useState, useEffect,useCallback} from 'react';
 
 import MoviesList from './components/MoviesList';
 import './App.css';
@@ -9,7 +9,7 @@ function App() {
   const [isLoading,setIsLoading] = useState(false);
   const [error,setError] = useState(null);
 
-  async function fetchMoviesHandler(){
+  const fetchMoviesHandler = useCallback(async()=>{
     setIsLoading(true);
     setError(null);
     try {
@@ -32,7 +32,12 @@ function App() {
       setError(error.message)
     } 
     setIsLoading(false)
-  }
+  },[])
+
+  useEffect(() =>{
+    fetchMoviesHandler();
+  },[fetchMoviesHandler])
+
 let content = <p>No Content</p>;
 if(movies.length > 0){
   content = <MoviesList movies={movies} ></MoviesList>
